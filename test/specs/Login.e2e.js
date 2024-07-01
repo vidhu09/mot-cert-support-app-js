5eb6e5e0318a526252ae6b31ba201dd3064172a7
Vidhya Anbalagan <vidhyalakshmi.anbalagan@bbc.co.uk>
2024-07-01 16:11:38 +0100

    fix element not found error in Login.e2e.js

--- test/specs/Login.e2e.js
+++ test/specs/Login.e2e.js
@@ -9,9 +9,9 @@ describe('My Login application', () => {
     await $('input[name="password"]').setValue('password123')
     await $('button').click()
 
-    const element = await $('.card-title')
-    console.log(await element.getText());
-    await expect(element).toHaveText('Projects')
+   //  const element = await $('.card-title');
+   //  console.log(await element.getText());
+    await expect($('.card-title')).toHaveText(expect.stringContaining('Projects'))
    })
 
 })
