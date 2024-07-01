e491fd68d81c7035c35baa426f59b61c8c9625d8
Vidhya Anbalagan <vidhyalakshmi.anbalagan@bbc.co.uk>
2024-07-01 14:53:11 +0100

    install spec reporter dev dependency

--- wdio.conf.js
+++ wdio.conf.js
@@ -126,7 +126,7 @@ exports.config =
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['dot', 'spec'],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
