8b3560fbd67917dd297276245aff543dd65e0c3b
Vidhya Anbalagan <vidhyalakshmi.anbalagan@bbc.co.uk>
2024-07-07 19:30:10 +0100

    Added reporting to Github Actions

--- wdio.conf.js
+++ wdio.conf.js
@@ -124,6 +124,12 @@ exports.config = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: ['spec'],
+    reporters: [['junit',{
+        outputDir: './reports/e2e-report/',
+        outputFileFormat: function(options) {
+            return 'junit-report.xml'
+        }
+    }]]
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
